b8ee4e54306720cb1e996fd6d6d7482febf5ff60
AlexV <asmodehn@gmail.com>
2021-02-16 10:41:33 +0100

    simple function and test, testing app setup.

--- apps/xest/lib/xest.ex
+++ apps/xest/lib/xest.ex
@@ -6,4 +6,11 @@ defmodule Xest do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def answer do
+    42
+  end
+
+
+
 end
---
+++ apps/xest/test/xest_test.exs
@@ -0,0 +1,9 @@
+defmodule Xest.Test do
+  use ExUnit.Case, async: true
+
+  require Xest
+
+  test "the answer" do
+    assert Xest.answer == 42
+  end
+end
