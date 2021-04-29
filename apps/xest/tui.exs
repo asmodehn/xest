89f36f15bb9b69088d22639ba9000e701f0a4117
AlexV <asmodehn@gmail.com>
2021-04-29 15:47:19 +0200

    add ratatouille for TUI attempts with example remove pre_commit config

---
+++ apps/xest/tui.exs
@@ -0,0 +1,23 @@
+defmodule Counter do
+  @behaviour Ratatouille.App
+
+  import Ratatouille.View
+
+  def init(_context), do: 0
+
+  def update(model, msg) do
+    case msg do
+      {:event, %{ch: ?+}} -> model + 1
+      {:event, %{ch: ?-}} -> model - 1
+      _ -> model
+    end
+  end
+
+  def render(model) do
+    view do
+      label(content: "Counter is #{model} (+/-)")
+    end
+  end
+end
+
+Ratatouille.run(Counter)
