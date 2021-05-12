1527f5d26b1f89f0fd965a46886ca6c98b0c630d
AlexV <asmodehn@gmail.com>
2021-05-12 18:48:54 +0200

    add transient map module and test

---
+++ apps/xest/lib/xest/transient_map.ex
@@ -0,0 +1,61 @@
+defmodule Xest.TransientMap do
+  @moduledoc """
+    A map that forgets its content after some time...
+  """
+
+  require Timex
+
+  @type key() :: any()
+  @type value() :: any()
+
+  defstruct birthdate: nil,
+            lifetime: nil,
+            store: %{}
+
+  @type t :: %__MODULE__{
+          birthdate: DateTime.t(),
+          lifetime: Time.t(),
+          store: Map.t()
+        }
+
+  def new(lifetime \\ ~T[00:05:00], birthdate \\ Xest.DateTime.utc_now()) do
+    # TODO : prevent negative lifetime ??
+    %__MODULE__{
+      lifetime: lifetime,
+      birthdate: birthdate
+    }
+  end
+
+  # TODO: Map API... # Note: Dict behaviour is deprecated...
+
+  @spec delete(map(), key()) :: map()
+  def delete(t, key) do
+    dead_or_alive(t)
+    |> Map.update!(:store, fn store -> Map.delete(store, key) end)
+  end
+
+  @spec fetch(map(), key()) :: {:ok, value()} | :error
+  def fetch(t, key) do
+    dead_or_alive(t) |> IO.inspect() |> Map.fetch!(:store) |> IO.inspect() |> Map.fetch(key)
+  end
+
+  @spec put(map(), key(), value()) :: map()
+  def put(t, key, value) do
+    dead_or_alive(t)
+    |> Map.update!(:store, fn store -> Map.put(store, key, value) end)
+  end
+
+  defp dead_or_alive(t) do
+    now = Xest.DateTime.utc_now()
+
+    case Timex.compare(
+           Timex.add(t.birthdate, Timex.Duration.from_time(t.lifetime)),
+           now
+         ) do
+      # greater cf. https://hexdocs.pm/timex/Timex.html#compare/3
+      1 -> t
+      # else build a new one with same lifetime...
+      _ -> new(t.lifetime, now)
+    end
+  end
+end
---
+++ apps/xest/test/xest/transient_map_test.exs
@@ -0,0 +1,110 @@
+defmodule Xest.TransientMap.Test do
+  use ExUnit.Case, async: true
+  use FlowAssertions
+
+  alias Xest.TransientMap
+
+  # cf https://medium.com/genesisblock/elixir-concurrent-testing-architecture-13c5e37374dc
+  import Hammox
+
+  @birthdate ~U[1970-01-02 12:34:56Z]
+  @lifetime ~T[00:05:00]
+  @valid_clock_time ~U[1970-01-02 12:37:56Z]
+  @invalid_clock_time ~U[1970-01-02 12:44:56Z]
+  @child_valid_clock_time ~U[1970-01-02 12:47:56Z]
+
+  describe "Given an empty transient map (with a clock)" do
+    setup :empty_transient_map
+
+    test "When next clock time is valid, Then we can store a key/value and retrieve the value with the key",
+         %{tmap: tmap} do
+      DateTimeMock
+      |> expect(:utc_now, fn -> @valid_clock_time end)
+      |> expect(:utc_now, fn -> @valid_clock_time end)
+
+      tmap = TransientMap.put(tmap, :new_key, "new_value")
+      assert TransientMap.fetch(tmap, :new_key) == {:ok, "new_value"}
+    end
+
+    test "When next clock time is invalid on put, Then we can still store a key/value and retrieve the value with the key",
+         %{tmap: tmap} do
+      DateTimeMock
+      |> expect(:utc_now, fn -> @invalid_clock_time end)
+      |> expect(:utc_now, fn -> @child_valid_clock_time end)
+
+      tmap = TransientMap.put(tmap, :new_key, "new_value")
+      assert TransientMap.fetch(tmap, :new_key) == {:ok, "new_value"}
+    end
+  end
+
+  describe "Given a transient map with one key value (and a clock)" do
+    setup :filled_transient_map
+
+    test "When next clock time is valid, Then we can retrieve existing value with the key",
+         %{tmap: tmap} do
+      DateTimeMock
+      |> expect(:utc_now, fn -> @valid_clock_time end)
+
+      assert TransientMap.fetch(tmap, :existing) == {:ok, "value"}
+    end
+
+    test "When next clock time is invalid, Then we cannot retrieve the value",
+         %{tmap: tmap} do
+      DateTimeMock
+      |> expect(:utc_now, fn -> @invalid_clock_time end)
+
+      IO.inspect(tmap)
+      assert TransientMap.fetch(tmap, :existing) == :error
+      # Note map is immutable, so key is still there in store...
+    end
+
+    test "When next clock time is valid, Then we can store a key/value and retrieve the value with the key",
+         %{tmap: tmap} do
+      DateTimeMock
+      |> expect(:utc_now, fn -> @valid_clock_time end)
+      |> expect(:utc_now, fn -> @valid_clock_time end)
+      |> expect(:utc_now, fn -> @valid_clock_time end)
+
+      tmap = TransientMap.put(tmap, :new_key, "new_value")
+      assert TransientMap.fetch(tmap, :new_key) == {:ok, "new_value"}
+      # Note the existing key is still there and accessible
+      assert tmap.store == %{new_key: "new_value", existing: "value"}
+      assert TransientMap.fetch(tmap, :existing) == {:ok, "value"}
+    end
+
+    test "When next clock time is invalid on put, Then we can still store a key/value and retrieve hte value with the key",
+         %{tmap: tmap} do
+      DateTimeMock
+      |> expect(:utc_now, fn -> @invalid_clock_time end)
+      |> expect(:utc_now, fn -> @child_valid_clock_time end)
+
+      tmap = TransientMap.put(tmap, :new_key, "new_value")
+      # Note the existing (now invalid) key is gone when we overwrote tmap
+      assert tmap.store == %{new_key: "new_value"}
+      assert TransientMap.fetch(tmap, :new_key) == {:ok, "new_value"}
+    end
+  end
+
+  defp empty_transient_map(_) do
+    DateTimeMock
+    |> expect(:utc_now, fn -> @birthdate end)
+
+    tmap = TransientMap.new(@lifetime)
+    IO.inspect(tmap)
+    %{tmap: tmap}
+  end
+
+  defp filled_transient_map(_test) do
+    # TODO : fixture with simpler in/out types...
+    %{tmap: tmap} = empty_transient_map(_test)
+
+    # adding a key value pair in valid time
+
+    DateTimeMock
+    |> expect(:utc_now, fn -> @valid_clock_time end)
+
+    tmap = TransientMap.put(tmap, :existing, "value")
+    IO.inspect(tmap)
+    %{tmap: tmap}
+  end
+end
