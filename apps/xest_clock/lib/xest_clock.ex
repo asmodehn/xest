7746c95f4daceb3e20d2aab8149f3309f8ffc6d6
AlexV <asmodehn@gmail.com>
2022-11-16 10:01:07 +0100

    xestclock implemented in the local usecase

--- apps/xest_clock/lib/xest_clock.ex
+++ apps/xest_clock/lib/xest_clock.ex
@@ -1,18 +1,39 @@
 defmodule XestClock do
   @moduledoc """
   Documentation for `XestClock`.
+
+  Design decisions:
+    - since we want to follow a server clock from anywhere, we use NaiveDateTime, and assume it to always be UTC
+    - since this is a "functional" library, we provide a data structure that the user can host in a process
   """
 
+  @enforce_keys []
+  defstruct remotes: %{},
+            system_clock_closure: &NaiveDateTime.utc_now/0
+
+  @typedoc "A naive clock, callable (impure) function returning a DateTime"
+  @type naive_clock() :: (() -> NaiveDateTime.t())
+
+  @typedoc "Remote NaiveDatetime struct"
+  @type t() :: %__MODULE__{
+          remotes: %{atom() => RemoteClock.t()},
+          system_clock_closure: naive_clock()
+        }
+
   @doc """
-  Hello world.
+  Returns the current (local or remote) naive datetime in UTC.
+
+  To get the local time, just use the default clock:
+    %XestClock{} |> XestClock.utc_now()
+  But it can also be customized for tests
 
   ## Examples
 
-      iex> XestClock.hello()
-      :world
+      %XestClock{system_clock_closure: fn ->  ~N[2010-04-17 14:00:00] end} |> XestClock.utc_now()
+       ~N[2010-04-17 14:00:00]
 
   """
-  def hello do
-    :world
+  def utc_now(%XestClock{} = clock, exchange \\ :local) do
+    clock.system_clock_closure.()
   end
 end
--- apps/xest_clock/test/xest_clock_test.exs
+++ apps/xest_clock/test/xest_clock_test.exs
@@ -2,7 +2,32 @@ defmodule XestClockTest do
   use ExUnit.Case
   doctest XestClock
 
-  test "greets the world" do
-    assert XestClock.hello() == :world
+  describe "XestClock" do
+    test "defaults to no remote and local naive utc_now closure" do
+      clk = %XestClock{}
+      assert clk.remotes == %{}
+      assert clk.system_clock_closure == (&NaiveDateTime.utc_now/0)
+    end
+
+    test "accepts different system closures for tests" do
+      clk = %XestClock{system_clock_closure: fn -> ~N[2010-04-17 14:00:00] end}
+      assert clk.remotes == %{}
+      assert clk.system_clock_closure.() == ~N[2010-04-17 14:00:00]
+    end
+  end
+
+  describe "utc_now/1" do
+    setup do
+      clk = %XestClock{system_clock_closure: fn -> ~N[2010-04-17 14:00:00] end}
+      %{clock: clk}
+    end
+
+    test "returns local now", %{clock: clk} do
+      assert XestClock.utc_now(clk) == ~N[2010-04-17 14:00:00]
+    end
+  end
+
+  describe "utc_now/2" do
+    test "returns exchange clock"
   end
 end
