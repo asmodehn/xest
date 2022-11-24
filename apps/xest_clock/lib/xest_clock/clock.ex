f5b80ef33c2c722af99b63ab30f41f01c4b7e335
AlexV <asmodehn@gmail.com>
2022-11-24 17:17:58 +0100

    add enumerable implementation for clock

--- apps/xest_clock/lib/xest_clock/clock.ex
+++ apps/xest_clock/lib/xest_clock/clock.ex
@@ -39,33 +39,175 @@ defmodule XestClock.Clock do
   @typedoc "XestClock.Clock struct"
   @type t() :: %__MODULE__{
           unit: System.time_unit(),
-          read: (() -> integer),
+          # Note: anonymous function of arity/2 can be enumerable... but nowadays Stream got a struct ??
+          #
+          read: Enumerable.t(),
           origin: atom
         }
 
+  @doc """
+    Creates a new clock struct that will repeatedly call System.monotonic_time
+  """
   @spec new(atom, System.time_unit()) :: t()
-  def new(origin, unit) do
+  def new(:local, unit) do
     unit = Timeunit.normalize(unit)
-
-    %__MODULE__{
-      unit: unit,
-      origin: origin,
-      read: fn -> System.monotonic_time(unit) end
-    }
+    new(:local, unit, fn -> System.monotonic_time(unit) end)
   end
 
+  @doc """
+    Creates a new clock struct that will
+      - repeatedly call read() if it is a function.
+      - unfold the list of integers if it is a list, returning one at a time on each tick().
+    read() output is dynamically verified to be ascending monotonically.
+    However, in the dynamic read() case, note that the first read happens immediately on creation
+    in order to get a first accumulator to compare the next with.
+  """
   @spec new(atom, System.time_unit(), (() -> integer)) :: t()
-  def new(origin, unit, read) do
+  def new(origin, unit, read) when is_function(read, 0) do
+    #    last_max = read.()
     %__MODULE__{
       unit: Timeunit.normalize(unit),
       origin: origin,
       read: read
+      #        Stream.concat(last_max, Stream.repeatedly(read))
+      #            |> Stream.transform(last_max, fn i, acc ->
+      #                # this dynamically verifies the list of values in the stream is monotonic
+      #                if i >= acc, do: {[i], i}, else: {:halt, acc}
+      #                # or halts the stream (an error has happened -> let it fail)
+      #            end)
     }
   end
 
-  def tick(%__MODULE__{} = clock) do
-    Timestamp.new(clock.origin, clock.unit, clock.read.())
+  @spec new(atom, System.time_unit(), [integer]) :: t()
+  def new(origin, unit, read) when is_list(read) do
+    %__MODULE__{
+      unit: Timeunit.normalize(unit),
+      origin: origin,
+      # TODO : is sorting before hand better ?? different behavior from repeated calls -> lazy impose skipping...
+      read: read
+      #        Stream.unfold(Enum.sort(read, :asc), fn
+      #        [] -> nil
+      #        [h, hh| l] -> {h,
+      #          cond do
+      #            hh >= h -> {h, [hh | l]}
+      #            hh < h -> {h, []}  # skip the non monotonic value and stops (same as the stream case)
+      #          end}
+      #      end)
+    }
   end
+
+  @doc """
+    This is not aimed for principal use. but it is useful to have for preplanned clocks,
+    to iterate on the list of ticks
+  """
+  def with_read(%__MODULE__{} = clock, new_read) when is_list(new_read) do
+    %__MODULE__{
+      unit: clock.unit,
+      origin: clock.origin,
+      read: new_read
+    }
+  end
+
+  def with_read(%__MODULE__{} = clock, new_read),
+    do: raise(ArgumentError, message: "#{new_read} is not a list. unsupported.")
+
+  @doc """
+    Implements the enumerable protocol for a clock, so that it can be used as a `Stream` (lazy enumerable).
+  """
+  defimpl Enumerable, for: __MODULE__ do
+    def count(_clock), do: {:error, __MODULE__}
+
+    def member?(_clock, _value), do: {:error, __MODULE__}
+
+    def slice(_clock), do: {:error, __MODULE__}
+
+    def reduce(_clock, {:halt, acc}, _fun), do: {:halted, acc}
+    def reduce(clock, {:suspend, acc}, fun), do: {:suspended, acc, &reduce(clock, &1, fun)}
+
+    # reduce in the case of a list. Guarantees monotonicity
+    #    def reduce(%XestClock.Clock{read: []} = clock, {:cont, acc}, _fun), do: {:done, acc}
+    #    # Note : We use similar structure are list implementation, however semantics are different
+    #    #  List : [[elem], count] But here we want  Clock : [[elem] | max]
+    #    def reduce(%XestClock.Clock{read: [head | tail]}, {:cont, [elem | max] = acc}, fun) do
+    #      # Here we delegate to the List implementation of reduce/3.
+    #      # It helps when double checking the algorithm...
+    ##      Be careful however of the semantics for the accumulator in List: [[elem], count]
+    #      IO.inspect(acc)
+    #      cond do
+    #            # lookahead to verify increasing monotonicity.
+    #            head >= max -> reduce(tail, fun.(head, [[],])), fun)
+    #            # otherwise skip the non monotonic value and stops (same as the stream case)
+    #            head < acc -> reduce(tail, fun.(head, acc) |> IO.inspect, fun)
+    #          end
+    #    end
+
+    defp timestamp(clock, read_value), do: Timestamp.new(clock.origin, clock.unit, read_value)
+
+    # because accumulator is not normalized between all Stream functions
+    # But we want to get information from it
+    defp first_in_acc(acc) when is_tuple(acc), do: elem(acc, 0)
+    defp first_in_acc(acc) when is_list(acc), do: hd(acc)
+    # TODO : keep adding cases when encountered
+    #  we want to get the last element returned by the stream, leveraging the accumulator
+
+    # NB : Here we **temporarily** overuse acc with extra structure, to guarantee monotonicity
+    # We assume hd(hd(acc)) is the last element observed by the stream.
+    # This is the case in List, and in our Record implementation.
+    def reduce(%XestClock.Clock{read: read} = clock, {:cont, acc}, fun) when is_function(read) do
+      #      IO.inspect(acc)
+      # get next tick.
+      tick = read.()
+
+      # verify increasing monotonicity with acc
+      cond do
+        # first case : get the tick
+        first_in_acc(acc) == [] ->
+          #              IO.inspect("empty acc case")
+          reduce(clock, fun.(timestamp(clock, tick), acc), fun)
+
+        tick >= hd(first_in_acc(acc)).ts ->
+          #              IO.inspect(" tick increase case")
+          # Note : here we forcefully drop the previous accumulated stream element
+          reduce(clock, fun.(timestamp(clock, tick), acc), fun)
+
+        tick < hd(first_in_acc(acc)).ts ->
+          #              IO.inspect("tick DECREASE case")
+          reduce(clock, {:halt, acc}, fun)
+      end
+    end
+
+    def reduce(%XestClock.Clock{read: []} = clock, {:cont, acc}, fun), do: {:done, acc}
+
+    def reduce(%XestClock.Clock{read: [tick | t]} = clock, {:cont, acc}, fun) do
+      #      IO.inspect(acc)
+
+      # verify increasing monotonicity with acc
+      cond do
+        # first case : get the tick
+        first_in_acc(acc) == [] ->
+          #              IO.inspect("empty acc case")
+          reduce(clock |> XestClock.Clock.with_read(t), fun.(timestamp(clock, tick), acc), fun)
+
+        tick >= hd(first_in_acc(acc)).ts ->
+          #              IO.inspect(" tick increase case")
+          # Note : here we forcefully drop the previous accumulated stream element
+          reduce(clock |> XestClock.Clock.with_read(t), fun.(timestamp(clock, tick), acc), fun)
+
+        tick < hd(first_in_acc(acc)).ts ->
+          #              IO.inspect("tick DECREASE case")
+          reduce(clock, {:halt, acc}, fun)
+      end
+    end
+  end
+
+  # note: this is a stream clock : no unique tick() !
+  # This would require to keep state in a process...
+  # and we want to bring that up to the user-level
+
+  #  @spec ticks(t()) :: Enumerable.t()
+  #  def ticks(%__MODULE__{} = clock) do
+  #    Stream.map(clock.read, fn ts -> Timestamp.new(clock.origin, clock.unit, ts) end)
+  #  end
 
   #  @doc """
   #  Initializes a remote clock, by specifying the unit in which the time value will be expressed
--- apps/xest_clock/test/xest_clock/clock_test.exs
+++ apps/xest_clock/test/xest_clock/clock_test.exs
@@ -2,10 +2,30 @@ defmodule XestClock.Clock.Test do
   use ExUnit.Case
   doctest XestClock.Clock
 
+  alias XestClock.Clock
+
+  @doc """
+  util function to always pattern match on timestamps
+  """
+  def ts_retrieve(origin, unit) do
+    fn ticks ->
+      ts_stream =
+        for t <- ticks do
+          %Clock.Timestamp{
+            origin: ^origin,
+            ts: ts,
+            unit: ^unit
+          } = t
+
+          ts
+        end
+    end
+  end
+
   describe "XestClock.Clock" do
     test "new(:local, time_unit) generates local clock with custom time_unit" do
       for unit <- [:second, :millisecond, :microsecond, :nanosecond] do
-        clock = XestClock.Clock.new(:local, unit)
+        clock = Clock.new(:local, unit)
         assert clock.origin == :local
         assert clock.unit == unit
       end
@@ -21,14 +41,33 @@ defmodule XestClock.Clock.Test do
       end)
     end
 
-    setup do
+    test "tick/1 returns increasing timestamp for local clock" do
+      for unit <- [:second, :millisecond, :microsecond, :nanosecond] do
+        clock = Clock.new(:local, unit)
+
+        ts_list = ts_retrieve(:local, unit).(clock |> Enum.take(2) |> Enum.to_list())
+
+        assert Enum.sort(ts_list, :asc) == ts_list
+      end
+    end
+
+    test "tick/1 using list of integers stops at the first integer that is not greater than the current one" do
+      clock = Clock.new(:testclock, :second, [1, 2, 3, 5, 4])
+
+      assert ts_retrieve(:testclock, :second).(clock |> Stream.take(5) |> Enum.to_list()) == [
+               1,
+               2,
+               3,
+               5
+             ]
+    end
+
+    test "tick/1 returns increasing timestamp for clock using agent update as read function" do
       #  A simple test ticker agent, that ticks everytime it is called
-      # TODO : use start_supervised
+      # TODO : use start_supervised ??
       {:ok, clock_agent} =
         Agent.start_link(fn ->
-          # The ticks as a sequence
-          [1, 2_000, 3_000_000, 4_000_000_000, 42]
-          # Note : for stream we need one more than retrieved...
+          [1, 2, 3, 5, 4]
         end)
 
       ticker = fn ->
@@ -38,43 +77,87 @@ defmodule XestClock.Clock.Test do
         )
       end
 
-      %{ticker: ticker}
-    end
+      # NB : using an agent to store state is NOT similar to Stream.unfold(),
+      # As all operations on a stream have to be done "at once",
+      # and cannot "tick by tick", as possible when an agent stores the state.
 
-    test "monotonic_time/2 returns clock time and convert between units", %{ticker: ticker} do
-      clock = XestClock.Clock.new(:local_testclock, :nanosecond, ticker)
+      # The agent usecase is similar to what happens with the system clock.
 
-      assert XestClock.Clock.monotonic_time(clock, :nanosecond) == 1
-      assert XestClock.Clock.monotonic_time(clock, :microsecond) == 2
-      assert XestClock.Clock.monotonic_time(clock, :millisecond) == 3
-      assert XestClock.Clock.monotonic_time(clock, :second) == 4
-    end
+      # However we *can encapsulate/abstract* the Agent (state-updating) request behaviour
+      # with a stream repeatedly calling and updating the agent (as with the system clock)
 
-    test "stream returns a stream", %{ticker: ticker} do
-      clock = XestClock.Clock.new(:local_testclock, :nanosecond, ticker)
+      clock = Clock.new(:testclock, :nanosecond, ticker)
 
-      assert XestClock.Clock.stream(clock, :nanosecond)
-             |> Stream.take(4)
-             |> Enum.to_list() == [
+      assert ts_retrieve(:testclock, :nanosecond).(clock |> Stream.take(5) |> Enum.to_list()) == [
                1,
-               2_000,
-               3_000_000,
-               4_000_000_000
-             ]
-    end
-
-    test "stream manages unit conversion", %{ticker: ticker} do
-      clock = XestClock.Clock.new(:local_testclock, :nanosecond, ticker)
-
-      assert XestClock.Clock.stream(clock, :microsecond)
-             |> Stream.take(4)
-             |> Enum.to_list() == [
-               # Note : only integer : lower precision is lost !
-               0,
                2,
-               3_000,
-               4_000_000
+               3,
+               5
              ]
     end
+
+    # TODO, but as a stream of ticks
+    #    test "tick/1 returns timestamp" do
+    #      clock = Clock.new(:local_testclock, :nanosecond, [1, 2_000, 3_000_000, 4_000_000_000, 42])
+    #
+    #      assert Clock.tick(clock) ==  %Clock.Timestamp{
+    #              origin: :local_testclock,
+    #              ts: 1,
+    #              unit: :nanosecond
+    #            }
+    #                  assert XestClock.Clock.tick(clock) ==  %Clock.Timestamp{
+    #              origin: :local_testclock,
+    #              ts: 2000,
+    #              unit: :nanosecond
+    #            }
+    #                              assert XestClock.Clock.tick(clock) ==  %Clock.Timestamp{
+    #              origin: :local_testclock,
+    #              ts: 3_000_000,
+    #              unit: :nanosecond
+    #            }
+    #                              assert XestClock.Clock.tick(clock) ==  %Clock.Timestamp{
+    #              origin: :local_testclock,
+    #              ts: 4_000_000_000,
+    #              unit: :nanosecond
+    #            }
+    #    end
+
+    #
+    #    test "monotonic_time/2 returns clock time and convert between units", %{ticker: ticker} do
+    #      clock = Clock.new(:local_testclock, :nanosecond, ticker)
+    #
+    #      assert Clock.monotonic_time(clock, :nanosecond) == 1
+    #      assert Clock.monotonic_time(clock, :microsecond) == 2
+    #      assert Clock.monotonic_time(clock, :millisecond) == 3
+    #      assert Clock.monotonic_time(clock, :second) == 4
+    #    end
+
+    #
+    #    test "stream returns a stream", %{ticker: ticker} do
+    #      clock = Clock.new(:local_testclock, :nanosecond, ticker)
+    #
+    #      assert Clock.stream(clock, :nanosecond)
+    #             |> Stream.take(4)
+    #             |> Enum.to_list() == [
+    #               1,
+    #               2_000,
+    #               3_000_000,
+    #               4_000_000_000
+    #             ]
+    #    end
+    #
+    #    test "stream manages unit conversion", %{ticker: ticker} do
+    #      clock = Clock.new(:local_testclock, :nanosecond, ticker)
+    #
+    #      assert Clock.stream(clock, :microsecond)
+    #             |> Stream.take(4)
+    #             |> Enum.to_list() == [
+    #               # Note : only integer : lower precision is lost !
+    #               0,
+    #               2,
+    #               3_000,
+    #               4_000_000
+    #             ]
+    #    end
   end
 end
