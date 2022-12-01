10cd90327d78f7751e0dc34918bd1e74267e819a
AlexV <asmodehn@gmail.com>
2022-12-01 11:46:15 +0100

    add genserver as test helper to test memory usage

--- apps/xest_clock/mix.exs
+++ apps/xest_clock/mix.exs
@@ -6,6 +6,9 @@ defmodule XestClock.MixProject do
       app: :xest_clock,
       version: "0.1.0",
       elixir: "~> 1.13",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      # TMP : warning shoudl be fixed !!!
+      elixirc_options: [warnings_as_errors: false],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -17,6 +20,12 @@ defmodule XestClock.MixProject do
       extra_applications: [:logger]
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  # to be able to interactively use test/support
+  defp elixirc_paths(:dev), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
