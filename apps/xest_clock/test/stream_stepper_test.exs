8e522e28554c3888269095b3b6258f63583b575f
AlexV <asmodehn@gmail.com>
2022-12-03 17:00:59 +0100

    add stream_stepper based on gen_Stage to extract stream content one by one

---
+++ apps/xest_clock/test/stream_stepper_test.exs
@@ -0,0 +1,116 @@
+defmodule XestClock.CheckServer.Test do
+  use ExUnit.Case
+  doctest XestClock.CheckServer
+
+  alias XestClock.Monotone
+  alias XestClock.StreamStepper
+
+  describe "StreamStepper" do
+    setup [:test_stream, :gen_stage_setup]
+
+    defp test_stream(%{usecase: usecase}) do
+      case usecase do
+        :const_fun ->
+          %{test_stream: Stream.repeatedly(fn -> 42 end)}
+
+        :list ->
+          %{test_stream: [5, 4, 3, 2, 1]}
+
+        :stream ->
+          %{
+            test_stream:
+              Stream.unfold(5, fn
+                0 -> nil
+                n -> {n, n - 1}
+              end)
+          }
+      end
+    end
+
+    defp gen_stage_setup(%{test_stream: test_stream}) do
+      # We use start_supervised! from ExUnit to manage gen_stage
+      # and not with the gen_stage :link option
+      streamstpr = start_supervised!({StreamStepper, {test_stream, []}})
+      %{streamstpr: streamstpr}
+    end
+
+    @tag usecase: :const_fun
+    test "with constant function in a Stream return value on next()",
+         %{streamstpr: streamstpr} do
+      before = Process.info(streamstpr)
+      current_value = StreamStepper.next(streamstpr)
+      after_compute = Process.info(streamstpr)
+
+      assert current_value == 42
+
+      # Memory stay constant
+      assert assert_constant_memory_reductions(before, after_compute) > 0
+    end
+
+    defp assert_constant_memory_reductions(before_reductions, after_reductions) do
+      assert before_reductions[:total_heap_size] == after_reductions[:total_heap_size]
+      assert before_reductions[:heap_size] == after_reductions[:heap_size]
+      assert before_reductions[:stack_size] == after_reductions[:stack_size]
+      # but reductions were processed
+      after_reductions[:reductions] - before_reductions[:reductions]
+    end
+
+    @tag usecase: :list
+    test "with List return value on next()",
+         %{streamstpr: streamstpr} do
+      before = Process.info(streamstpr)
+
+      assert StreamStepper.next(streamstpr) == 5
+
+      first = Process.info(streamstpr)
+
+      # Memory stay constant
+      assert assert_constant_memory_reductions(before, first) > 0
+
+      assert StreamStepper.next(streamstpr) == 4
+
+      second = Process.info(streamstpr)
+
+      # Memory stay constant
+      assert assert_constant_memory_reductions(first, second) > 0
+
+      assert StreamStepper.next(streamstpr) == 3
+
+      assert StreamStepper.next(streamstpr) == 2
+
+      assert StreamStepper.next(streamstpr) == 1
+
+      assert StreamStepper.next(streamstpr) == nil
+      # Note : the Process is still there (in case more data gets written into the stream...)
+    end
+
+    @tag usecase: :stream
+    test "with Stream.unfold() return value on next()",
+         %{streamstpr: streamstpr} do
+      before = Process.info(streamstpr)
+
+      assert StreamStepper.next(streamstpr) == 5
+
+      first = Process.info(streamstpr)
+
+      # Memory stay constant
+      assert assert_constant_memory_reductions(before, first) > 0
+
+      assert StreamStepper.next(streamstpr) == 4
+
+      second = Process.info(streamstpr)
+
+      # Memory stay constant
+      assert assert_constant_memory_reductions(first, second) > 0
+
+      assert StreamStepper.next(streamstpr) == 3
+
+      assert StreamStepper.next(streamstpr) == 2
+
+      assert StreamStepper.next(streamstpr) == 1
+
+      assert StreamStepper.next(streamstpr) == nil
+      # Note : the Process is still there (in case more data gets written into the stream...)
+    end
+  end
+end
---
+++ apps/xest_clock/test/support/stream_stepper.ex
@@ -0,0 +1,84 @@
+defmodule XestClock.StreamStepper do
+  # Designed from GenStage.Streamer
+  @moduledoc """
+  This is a GenStage, abused to hold a stream (designed from GenStage.Streamer as in Elixir 1.14)
+    and setup so that a client process can ask for one element at a time, synchrounously.
+  We attempt to keep the same semantics, so the synchronous request will immediately trigger an event to be sent to all subscribers.
+  """
+
+  use GenStage
+
+  def start_link({stream, opts}) do
+    {:current_stacktrace, [_info_call | stack]} = Process.info(self(), :current_stacktrace)
+    GenStage.start_link(__MODULE__, {stream, stack, opts}, opts)
+  end
+
+  def init({stream, stack, opts}) do
+    continuation =
+      &Enumerable.reduce(stream, &1, fn
+        x, {acc, 1} -> {:suspend, {[x | acc], 0}}
+        x, {acc, counter} -> {:cont, {[x | acc], counter - 1}}
+      end)
+
+    {:producer, {stack, continuation}, Keyword.take(opts, [:dispatcher, :demand])}
+  end
+
+  ### Addendum, shortcut to get stream synchronously as in functional code
+  def next(pid \\ __MODULE__) do
+    GenServer.call(pid, :next)
+  end
+
+  def handle_call(:next, _from, {stack, continuation}) when is_atom(continuation) do
+    # nothing produced, returns nil in this case...
+    {:reply, nil, {stack, continuation}}
+  end
+
+  def handle_call(:next, _from, {stack, continuation}) do
+    # Ref: https://hexdocs.pm/gen_stage/GenStage.html#c:handle_call/3
+    # we immediately return the result of the computation,
+    # but we also set it to be dispatch as an event (other subscribers ?),
+    # just as a demand of 1 would have.
+    case continuation.({:cont, {[], 1}}) do
+      {:suspended, {[], 0}, continuation} ->
+        {:reply, nil, [], {stack, continuation}}
+
+      {:suspended, {list, 0}, continuation} ->
+        {:reply, hd(list), :lists.reverse(list), {stack, continuation}}
+
+      {status, {[], _}} ->
+        GenStage.async_info(self(), :stop)
+        {:reply, nil, [], {stack, status}}
+
+      {status, {list, _}} ->
+        GenStage.async_info(self(), :stop)
+        {:reply, hd(list), :lists.reverse(list), {stack, status}}
+    end
+  end
+
+  ###
+
+  def handle_demand(_demand, {stack, continuation}) when is_atom(continuation) do
+    {:noreply, [], {stack, continuation}}
+  end
+
+  def handle_demand(demand, {stack, continuation}) when demand > 0 do
+    case continuation.({:cont, {[], demand}}) do
+      {:suspended, {list, 0}, continuation} ->
+        {:noreply, :lists.reverse(list), {stack, continuation}}
+
+      {status, {list, _}} ->
+        GenStage.async_info(self(), :stop)
+        {:noreply, :lists.reverse(list), {stack, status}}
+    end
+  end
+
+  def handle_info(:stop, state) do
+    {:stop, :normal, state}
+  end
+
+  def handle_info(msg, {stack, continuation}) do
+    log = '** Undefined handle_info in ~tp~n** Unhandled message: ~tp~n** Stream started at:~n~ts'
+    :error_logger.warning_msg(log, [inspect(__MODULE__), msg, Exception.format_stacktrace(stack)])
+    {:noreply, [], {stack, continuation}}
+  end
+end
