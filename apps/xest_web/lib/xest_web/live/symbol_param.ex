c7efcb1b21aed81e112507cbebd536710032ed5e
AlexV <asmodehn@gmail.com>
2023-02-06 12:23:31 +0100

    fix xest_Web after phoenix upgrade

--- apps/xest_web/lib/xest_web/live/symbol_param.ex
+++ apps/xest_web/lib/xest_web/live/symbol_param.ex
@@ -1,13 +1,14 @@
 defmodule XestWeb.SymbolParam do
   @moduledoc false
 
+  import Phoenix.Component
   alias Phoenix.LiveView
 
   def assign_symbol(socket, params) do
     case params do
       %{"symbol" => symbol} ->
         # assign exchange to socket if valid, otherwise redirects
-        socket |> LiveView.assign(symbol: symbol)
+        socket |> assign(symbol: symbol)
 
       _ ->
         socket |> LiveView.put_flash(:error, "symbol uri param not found")
